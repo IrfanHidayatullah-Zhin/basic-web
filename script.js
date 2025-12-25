0c1d972a2050b507282b482eecd530be8b93b066
IrfanHidayatullah-Zhin <irfanhidayatullah912@gmail.com>
2025-12-26 00:15:46 +0700

    Add counter feature and JS logic

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const btn = document.getElementById("btnChange");
 btn.addEventListener("click", () => {
   title.textContent = "Saya Sedang Belajar JavaScript!";
 });
+
+// hari ke 3 counter
+
+const countEl = document.getElementById("count");
+const btnPlus = document.getElementById("btnPlus");
+const btnReset = document.getElementById("btnReset");
+
+let count = 0;
+
+function updateCount() {
+  countEl.textContent = count;
+} 
+
+btnPlus.addEventListener("click", () => {
+  count++;
+  updateCount();
+} );
+
+btnReset.addEventListener("click", () => {
+  count = 0;
+  updateCount();
+} );

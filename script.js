2089873178032303a162f37c6a2b6b5d92ca63e2
IrfanHidayatullah-Zhin <irfanhidayatullah912@gmail.com>
2025-12-27 00:47:38 +0700

    Refactor CSS for cleaner UI layout

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ console.log("JS jalan");
 
 const title = document.getElementById("title");
 const btn = document.getElementById("btnChange");
+const nameList = document.getElementById("nameList");
+const nameForm = document.getElementById("nameForm");
+const btnAdd = document.getElementById("btnAdd");
 
 btn.addEventListener("click", () => {
   title.textContent = "Saya Sedang Belajar JavaScript!";
@@ -14,6 +17,7 @@ const btnPlus = document.getElementById("btnPlus");
 const btnReset = document.getElementById("btnReset");
 
 let count = 0;
+let names = []; // array untuk simpan data
 
 function updateCount() {
   countEl.textContent = count;
@@ -28,3 +32,27 @@ btnReset.addEventListener("click", () => {
   count = 0;
   updateCount();
 } );
+
+btnAdd.addEventListener("click", () => {
+  const value = nameForm.value.trim();
+
+  // validasi sederhana
+  if (value === "") {
+    alert("Nama tidak boleh kosong!");
+    return;
+  }
+
+  names.push(value);       // simpan ke array
+  nameForm.value = "";    // reset input
+  renderNames();          // update tampilan
+});
+
+function renderNames() {
+  nameList.innerHTML = ""; // bersihkan list
+
+  names.forEach((name) => {
+    const li = document.createElement("li");
+    li.textContent = name;
+    nameList.appendChild(li);
+  });
+}
